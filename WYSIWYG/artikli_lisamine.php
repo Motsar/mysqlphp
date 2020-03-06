6b28315810f86b306cd531a3a0d878e8bfbd044a
Motsar <martin.motsar@gmail.com>
2020-03-06 20:52:47 +0200

    ckeditori lisamine

---
+++ WYSIWYG/artikli_lisamine.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WYSIWYG</title>
+    <style type="text/css">
+        #sisu{
+            width: 700px;
+        }
+    </style>
+    <script src="ckeditor/ckeditor.js"></script>
+</head>
+<body>
+<div id="sisu">
+    <h1>Artikli lisamine</h1>
+
+    <form action="" method="post">
+			<textarea class="ckeditor" cols="20" name="uudis" id="uudis" rows="10">
+            </textarea>
+        <br>
+        <input type="submit" value="Salvesta">
+    </form>
+</div>
+
+</body>
+</html>
